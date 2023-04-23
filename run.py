8bf9d1f5e7fc3b89ef6a6d90d8ad38d649da21e0
gennadiy-gaysha <gennadiy.gaysha@gmail.com>
2023-04-23 21:46:15 +0000

    Add color fonts to the game

--- run.py
+++ run.py
@@ -15,40 +15,58 @@
 # 7. The player continues to guess until they correctly guess the entire
 # number (i.e., four bulls).
 from random import randint
+from colorama import Fore, Back, Style
+
+# COLOR VARIABLES
+
+line_color = Fore.LIGHTBLUE_EX
+rules_color = Fore.LIGHTYELLOW_EX
+err_color = Fore.RED                
+win_bcolor = Back.RED
+win_fcolor = Fore.LIGHTWHITE_EX              
+variable_color = Fore.LIGHTYELLOW_EX       
+dim = Style.DIM                     
+reset_all = Style.RESET_ALL            
 
 
 def game_rules():
-    print('''
--------------------------------------------------------------------------
+    print(line_color + '''-------------------------------------------------------------------------
 Welcome to brainstorming BULLS AND COWS GAME!!!
--------------------------------------------------------------------------
-The rules of Bulls and Cows:
+-------------------------------------------------------------------------''' + reset_all)
+    print(rules_color + '''The rules of Bulls and Cows:
+
 1. The game is played with a 4-digit number, in which each digit is unique.
+
 2. The computer chooses a random number, which the player tries to guess.
+
 3. The player makes a guess by entering a 4-digit number.
+
 4. If the player guesses a digit that is in the correct position in the
 chosen number, it is called a "bull".
+
 5. If the player guesses a digit that is in the chosen number, but in a
 different position, it is called a "cow".
+
 6. The computer responds to each guess with the number of bulls and cows.
 For example, if the chosen number is 4827 and the player guesses 1234, the
 computer would respond with "0 bulls, 2 cow" because the player guessed the
 digit 2, which is in the chosen number, but in a different position.
+
 7. The player continues to guess until they correctly guess the entire
-number (i.e., four bulls).
--------------------------------------------------------------------------
+number (i.e., four bulls).''' + reset_all)
+    print(line_color + '''-------------------------------------------------------------------------
 The computer has just generated a number that you need to guess. Give it a try!
--------------------------------------------------------------------------''')
+-------------------------------------------------------------------------''' + reset_all)
 
 
 game_rules()
 
 
 def number_generator():
-    generated_number = []
     '''
     Function generates 4-digit number (list of 4 random unique digits)
     '''
+    generated_number = []
     while len(generated_number) < 4:
         a = randint(0, 9)
         if a not in generated_number:
@@ -68,7 +86,6 @@ class Guess:
     contains a list of digits of the secret number.
     Contains class attribute guess_list which adds to list instance attributes
     after each initialization
-
     '''
     guess_list = []
 
@@ -137,41 +154,41 @@ def validate_guess(values):
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 4 values.
-    '''   
+    '''
     try:
         if len([i for i in values if not i.isdigit()]) > 0:
-            raise ValueError(' , '.join([i for i in values if not i.isdigit()]))
+            raise ValueError(f'your input contains unacceptable character(s): {variable_color}{" , ".join([i for i in values if not i.isdigit()])}{reset_all}')
     except ValueError as err:
-        print(f'Invalid data: your input contains {err} character(s). Try again please.')
+        print(f'Invalid data: {err_color}{err}{reset_all}. Try again please.')
         return False
 
     try:
         if len(values) != 4 and len(values) != 0:
             raise ValueError(
-                f'exactly 4 values required, you provided {len(values)}')
+                f'exactly 4 values required, you provided {variable_color}{len(values)}{reset_all}')
     except ValueError as err:
-        print(f'Invalid data: {err}. Try again please.\n')
+        print(f'Invalid data: {err_color}{err}{reset_all}. Try again please.\n')
         return False
 
     try:
         if len(values) == 0:
-            raise ValueError('you should not leave an empty field.')
+            raise ValueError('you should not leave an empty field')
     except ValueError as err:
-        print(f'Invalid data: {err}. Try again please.\n')
+        print(f'Invalid data: {err_color}{err}{reset_all}. Try again please.\n')
         return False
 
     try:
         if values in list(guess_list):
-            raise ValueError(values)
+            raise ValueError(f'you have already make this guess: {variable_color}{values}{reset_all}')
     except ValueError as err:
-        print(f'Invalid data: you have already make this guess: {err}. Try again please.')
+        print(f'Invalid data: {err_color}{err}{reset_all}. Try again please.')
         return False
 
     try:
         if len(values) != len(set(values)):
-            raise ValueError(' and '.join(str(x) for x in set([i for i in values if values.count(i) > 1])))
+            raise ValueError(f"you have repetetive digit(s) in your guess: {variable_color}{' and '.join(str(x) for x in {i for i in values if values.count(i) > 1})}{reset_all}")
     except ValueError as err:
-        print(f'Invalid data: you have repetetive digit(s) in your guess: {err}. Try again please.')
+        print(f'Invalid data: {err_color}{err}{reset_all}. Try again please.')
         return False
 
     return True
@@ -191,10 +208,11 @@ def main():
         guess.show_all_scores()
         if guess.bull_counter() == 4:
             break
-    print('''
-Congratulations! You win!!!
-Input Y if you want to play again. To quit the game input N
-''')
+    print()
+    print(win_bcolor + 'Congratulations! You win!!!' + reset_all)
+    print('''-------------------------------------------------------------------------
+Input Y if you want to play again. To quit the game input N.
+-------------------------------------------------------------------------''')
     while True:
         users_choice = input()
         if users_choice.lower() == 'y':
@@ -204,8 +222,8 @@ Input Y if you want to play again. To quit the game input N
             return False
         if users_choice.lower() == 'n':
             return False
-        print("Invalid input. Please input Y or N to confirm your choice")
-
+        print(err_color + f"Invalid input {variable_color}{users_choice}{reset_all}. " + err_color + "Please input Y or N to confirm your choice" + reset_all)
+    
 
 main()
 
